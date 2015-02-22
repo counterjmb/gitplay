5881b08f2dc5050daac6e31baccea0d854616e20
counterjmb <counterjmb@gmail.com>
2015-02-22 01:51:08 -0600

    branch1 stuff

---
+++ src/gitplay/Merge.java
@@ -0,0 +1,12 @@
+package gitplay;
+
+public class Merge {
+
+	public static void main(String[] args) {
+		
+		String branch1 = "this is from branch1";
+		
+		
+	}
+
+}
---
+++ src/gitplay/branch1code/branch1code.java
@@ -0,0 +1,7 @@
+package gitplay.branch1code;
+
+public class branch1code {
+	
+	public String branch = "branch 1";
+
+}
